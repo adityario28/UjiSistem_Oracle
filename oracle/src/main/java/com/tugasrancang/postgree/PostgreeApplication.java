997333f18638a7a3f9a324b6b937586575c17f58
Mahesa Rio Aditya <Adityario28@gmail.com>
2023-06-30 18:54:36 +0700

    Initial commit

---
+++ oracle/src/main/java/com/tugasrancang/postgree/PostgreeApplication.java
@@ -0,0 +1,29 @@
+package com.tugasrancang.postgree;
+
+import static com.tugasrancang.postgree.CreateTable.CreateAllTable;
+import static com.tugasrancang.postgree.InsertTable.InsertAllTable;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgreeApplication implements CommandLineRunner{
+//    //Postgre
+//        static final String DB_URL = "jdbc:postgresql://localhost:5432/public";
+//        static final String USER = "postgres";
+//        static final String PASS = "agung2002";
+    //Oracle
+        static final String DB_URL = "jdbc:oracle:thin:@localhost:1523/orcl";
+        static final String USER = "system";
+        static final String PASS = "agung2002";
+        
+	public static void main(String[] args) {
+		SpringApplication.run(PostgreeApplication.class, args);
+	}
+
+        @Override
+        public void run(String... args) throws Exception {
+//            System.out.println(CreateAllTable(DB_URL,USER,PASS));
+            System.out.println(InsertAllTable(DB_URL,USER,PASS));
+        }
+}
